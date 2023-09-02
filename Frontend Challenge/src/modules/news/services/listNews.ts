57470b00f2863ae59dd64aa46cd7a9e7138e26fb
Irfan Nugraha <irfan.nugraha404@gmail.com>
2023-09-02 17:30:24 +0700

    add header to allow cors

--- Frontend Challenge/src/modules/news/services/listNews.ts
+++ Frontend Challenge/src/modules/news/services/listNews.ts
@@ -4,7 +4,14 @@ import api from "../constants/api";
 const listNews = async (query: string) => {
   try {
     const response = await axios.get(
-      `${api.list}?country=${query}&apiKey=${process.env.REACT_APP_API_KEY}`
+      `${api.list}?country=${query}&apiKey=${process.env.REACT_APP_API_KEY}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
     );
 
     return response
